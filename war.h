0b8aa0bf2077be4164ed8ca22c0b7ba726999e1d
Eduardo <eduardoherrerabarra@gmail.com>
2025-04-14 09:38:28 -0400

    finished header files

--- war.h
+++ war.h
@@ -1,1 +1,32 @@
 #pragma once
+#include <iostream>
+#include "deck.h"
+#include <queue>
+using namespace std;
+
+
+
+
+
+class War
+{
+    //Constructor and Destructor
+    public:
+        War();
+        ~War();
+    
+    //PLAY!
+    public:
+        void play();
+    
+    protected:
+        queue<Card*> Deck_Player;
+        queue<Card*> Deck_Computer;
+
+        void Deal_Cards (Deck& deck);
+        void Play_Round();
+        void War_War(vector<Card*>& pile);
+        bool Continue_Questionmark();
+    
+
+};
